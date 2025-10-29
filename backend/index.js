633d3953a63b3dbcaf045fda0418ac701d81bfe8
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-30 02:13:15 +0530

    set express

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,9 @@
 const express=require("express")
 const cors=require("cors")  //it is use for fetch data from database in frontend
+
+//app config
+const app=express();
+const port = process.env.PORT || 2000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
