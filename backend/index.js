b8874373cf19c188e4a6a484a4b41fe0495a5501
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-30 02:14:28 +0530

    set mongoose

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,18 @@ const port = process.env.PORT || 2000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
+
+
+//database connection
+const mongoose = require("mongoose");
+ const connectDB = async () => {
+    try {
+      await mongoose.connect("");
+      console.log("Database connected successfully");
+    } catch (error) {
+      console.error("Error connecting to database:", error);
+      process.exit(1);
+    }
+  };
+//db connectin model
+connectDB();
