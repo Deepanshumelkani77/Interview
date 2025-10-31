4797f42aaac7e2d17f16b44ac1f122b5376827fb
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-31 12:55:18 +0530

    try to image

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import {assets} from '../assets/assets.js'
+
 
 const Image = () => {
   return (
     <div>
-      
+      <img src={assets.img} alt="Description" />
     </div>
   )
 }
+
+
 
 export default Image
