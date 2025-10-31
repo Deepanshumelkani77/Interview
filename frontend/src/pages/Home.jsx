22fdcd46e38d6828c199a6e8eb48730c2675bc1d
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-31 15:07:46 +0530

    image update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Image from '../components/Image'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
     <div>
-      <Image />
+      <Header />
     </div>
   )
 }
