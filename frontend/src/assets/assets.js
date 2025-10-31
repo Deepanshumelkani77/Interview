dad2d277e874903a71cfd04f60b32d62a0e730c1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-11-01 01:26:22 +0530

    set assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,13 +6,42 @@ import c from "./c.png"
 import d from "./d.png"
 import e from "./e.png"
 
+import p1 from "./p1.png"
+import p2 from "./p2.png"
+import p3 from "./p3.png"
+import p4 from "./p4.png"
+import p5 from "./p5.png"
+import p6 from "./p6.png"
+import p7 from "./p7.png"
+import p8 from "./p8.png"
+import p9 from "./p9.png"
+import p10 from "./p10.png"
+import p11 from "./p11.png"
+import p12 from "./p12.png"
+import p13 from "./p13.png"
+import p14 from "./p14.png"
+
 const assets = {
   img,
   a,
   b,
   c,
   d,
-  e
+  e,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14
 }
 
 export default assets
